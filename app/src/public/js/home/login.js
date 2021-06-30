230e343cba5419702ee41b5f6bcd242e7f29fe4e
binssa <bin.sa0823@gmail.com>
2021-06-30 21:36:35 +0900

    Save Object using DOM

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -2,3 +2,18 @@
 
 console.log("Hello");
 console.log("Bye!");
+//Login Function Implement
+const id = document.querySelector("#id");
+const pw = document.querySelector("#pw");
+const loginBtn = document.querySelector("button");
+
+// Button Event(Click)
+loginBtn.addEventListener("click", login);
+
+function login(){
+    const req = {
+        id : id.value,
+        pw : pw.value,
+    };
+    console.log(req);
+};
