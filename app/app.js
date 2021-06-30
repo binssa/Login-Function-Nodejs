a2c3a92f0ad3fec36577d6661c1275a69fe9e5b0
binssa <bin.sa0823@gmail.com>
2021-06-30 20:11:25 +0900

    Folder Structure

--- app/app.js
+++ app/app.js
@@ -4,10 +4,10 @@ const express = require("express");
 const app = express();
 
 // Routing
-const home = require("./routes/home")
+const home = require("./src/routes/home")
 
 // App Setting
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 app.use("/", home); // Middleware Config
