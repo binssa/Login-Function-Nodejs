76d988ba5cc76da1d65f9c91d5e0ab1073dd95af
binssa <bin.sa0823@gmail.com>
2021-07-01 20:38:50 +0900

    로그인 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,5 +1,10 @@
 "use strict"
 
+// 인증 시 필요한 계정 정보 
+const users = {
+    id: ["shb", "abc", "bin"],
+    pw: ["123", "1234", "12345"], 
+}
 // Modularity
 const output = {
     hello: (req, res) => {
@@ -13,6 +18,22 @@ const output = {
 const process = {
     login: (req, res) => {
         console.log(req.body);
+        const id = req.body.id;
+        const pw = req.body.pw;
+
+        if (users.id.includes(id)){
+            const idx = users.id.indexOf(id);
+            if (users.pw[idx] === pw){
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg: "로그인에 실패하셨습니다.",
+        })
     },
 };
 
