b0f5581fdd0df7c575beb57c588118329713d2c0
binssa <bin.sa0823@gmail.com>
2021-07-03 15:58:19 +0900

    회원가입 라우팅 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -11,11 +11,16 @@ const registerBtn = document.querySelector("#button");
 registerBtn.addEventListener("click", register);
 
 function register(){
+    if (!id.value){
+        return alert("ID를 입력해주세요.");
+    }
+    if (pw !== confirmPw){
+        return alert("비밀번호가 일치하지 않습니다.");
+    }
     const req = {
         id : id.value,
         name : name.value,
         pw : pw.value,
-        confirmPw : confirmPw.value,
     };
     // console.log(req);
     fetch("/register", {
